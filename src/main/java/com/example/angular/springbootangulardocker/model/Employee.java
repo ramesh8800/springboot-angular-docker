4aa6d57603985718414e851a15765b9cc5a72aee
ramesh8800 <42619259+ramesh8800@users.noreply.github.com>
2020-12-04 13:09:56 +0530

    Update Employee.java

--- src/main/java/com/example/angular/springbootangulardocker/model/Employee.java
+++ src/main/java/com/example/angular/springbootangulardocker/model/Employee.java
@@ -1,6 +1,7 @@
-package com.example.angular.springbootangulardocker.model;
+package com.example.angular.springbootcrudapi.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
